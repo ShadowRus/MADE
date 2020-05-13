713207c982b10651a1ded75bd1952c962aba5643
ShadowRus <51329746+ShadowRus@users.noreply.github.com>
2020-05-13 20:09:21 +0300

    Update blend.py

--- contest1/unsupervised-landmarks-thousand-landmarks-contest/blend.py
+++ contest1/unsupervised-landmarks-thousand-landmarks-contest/blend.py
@@ -1,21 +1,13 @@
 import pandas as pd
 from tqdm import tqdm
 
-sub = ['../HW1/Submissions/sub_resnet101.csv','../HW1/Submissions/sub_resnet152.csv']
+sub1=pd.read_csv('../HW1/Submissions/sub_resnet101.csv')
+sub2=pd.read_csv('../HW1/Submissions/sub_resnet152.csv')
 
-dfs_list = []
-
-for path in sub:
-    dfs_list.append(pd.read_csv(path))
-
-columns = dfs_list[0].columns[1:]
+columns = sub1.columns[1:]
 
 for col_name in tqdm(columns):
-    a = dfs_list[0][col_name]
-    for df in dfs_list[1:]:
-        a += df[col_name]
-    a /= len(dfs_list)
+    a = (sub1[col_name]+sub2[col_name])/2
+    sub1[col_name] =round(a).astype('int32')
 
-    dfs_list[0][col_name] = round(a).astype('int32')
-
-dfs_list[0].to_csv('blend_Rusakov.csv', index=False)
+sub1.to_csv('blend_Rusakov.csv', index=False)
